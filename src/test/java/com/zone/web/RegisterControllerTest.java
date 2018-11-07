ea7f5727b08e5f203da58ea9bb2b38a43a617f52
Ivan Gajev <ivan.gajev@gmail.com>
2018-11-07 17:58:49 +0200

    Update of warning for RegisterControllerTest

--- src/test/java/com/zone/web/RegisterControllerTest.java
+++ src/test/java/com/zone/web/RegisterControllerTest.java
@@ -4,7 +4,6 @@ import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
