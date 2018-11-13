972d386d47e9d0d9967406616da59c416b4803b9
Ivan Gajev <ivan.gajev@gmail.com>
2018-11-13 11:54:53 +0200

    Addition of Spring's Security

---
+++ src/main/java/com/zone/config/SecurityConfig.java
@@ -0,0 +1,41 @@
+package com.zone.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+	DataSource dataSource;
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth
+		.jdbcAuthentication()
+		.dataSource(dataSource)
+		.usersByUsernameQuery("select username, password, true from weighters where username=?")
+		.authoritiesByUsernameQuery("select username, 'ROLE_USER' from weighters where username=?");
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+		.authorizeRequests()
+		//TODO: Remove the next line, when authentication is ready for implementations 
+		//.regexMatchers("/profile.*").authenticated()
+		.anyRequest().permitAll()
+		.and()
+		.formLogin().and()
+		.httpBasic().and()
+		.rememberMe()
+		.tokenValiditySeconds(14400); //token valid 4 hours.
+		}
+}
--- src/main/java/com/zone/config/WeighterWebAppInitializer.java
+++ src/main/java/com/zone/config/WeighterWebAppInitializer.java
@@ -6,7 +6,7 @@ public class WeighterWebAppInitializer extends AbstractAnnotationConfigDispatche
 
 	@Override
 	protected Class<?>[] getRootConfigClasses() {
-		return new Class<?>[] { DataConfig.class };
+		return new Class<?>[] { DataConfig.class, SecurityConfig.class };
 	}
 
 	@Override
